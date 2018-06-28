3e63e8df016a7d0ecb867ec31e62f43771325d08
syd11846 <syd11846@ly.com>
2018-06-28 14:34:25 +0800

    autopublish

--- main.go
+++ main.go
@@ -6,12 +6,8 @@ import (
     "log"
 )
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm() //解析参数，默认是不会解析的 
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    } 
-    fmt.Fprintf(w, "Hello Wrold!") //这个写入到w的是输出到客户端的
+	r.ParseForm() //解析参数，默认是不会解析的 
+    fmt.Fprintf(w,	r.Body); 
 }
 func main() {
     http.HandleFunc("/", sayhelloName) //设置访问的路由
