c61c4279bbd6a6b24ad3496cb3c98683b57fc884
syd11846 <syd11846@ly.com>
2018-06-28 14:45:19 +0800

    publish

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 import (
     "fmt"
-    "net/http"
-    "strings"
+    "net/http" 
     "log"
 )
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() //解析参数，默认是不会解析的 
-    fmt.Fprintf(w,	r.Body); 
+	var bodyreader = r.Body;
+	var bodt []byte;
+	bodyreader.Read(bodt);
+    fmt.Fprintf(w,	string(bodt)); 
 }
 func main() {
     http.HandleFunc("/", sayhelloName) //设置访问的路由
